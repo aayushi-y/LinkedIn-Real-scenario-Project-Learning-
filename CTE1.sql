11b910afac9d5aa82af74791fd3387d1bddc9613
aayushi-y <33841859+aayushi-y@users.noreply.github.com>
2023-09-20 12:10:38 +0530

    Create CTE1.sql

---
+++ CTE1.sql
@@ -0,0 +1,17 @@
+/*CTE 1.1 and CTE 1.2*/
+WITH temp as (select MONTH(s.ORDERDATE) as monthly,
+                    s.PRODUCTID, SUM(s.REVENUE) AS total_revenue
+                FROM Subscriptions s
+                    WHERE YEAR(s.ORDERDATE) = 2022
+                        GROUP BY monthly,s.PRODUCTID
+)
+select 
+    p.PRODUCTNAME,
+    MAX(t.total_revenue) AS MAX_REV, 
+    MIN(t.total_revenue) AS MIN_REV, 
+    AVG(t.total_revenue) AS AVG_REV,
+    STDDEV(t.total_revenue) AS STD_DEV_REV
+    from temp t JOIN products p
+    ON t.PRODUCTID = p.PRODUCTID
+    GROUP BY p.PRODUCTNAME;
+            
