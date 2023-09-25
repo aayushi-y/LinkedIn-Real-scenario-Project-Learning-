dc188f8e26030fb5d7a0a379651076cb90cf92b0
aayushi-y <33841859+aayushi-y@users.noreply.github.com>
2023-09-25 12:25:55 +0530

    Update 8.sql

--- 8.sql
+++ 8.sql
@@ -13,3 +13,26 @@ from monthly_cte current
 monthly_cte previous
 where datediff('month', previous.month, current.month) = 1 AND 
 (current.revenue - previous.revenue) > 0
+/* WITH LAG WINDOW FUNCTION SAME QUERY CAN BE WRITTEN AS 
+
+With temp1 as (select date_trunc('month', orderdate) as order_date, 
+sum(revenue) as revenue from subscriptions 
+group by order_date
+order by order_date),
+temp2 AS (
+select order_date AS current_month, revenue AS current_revenue, 
+LAG(revenue, 1 ) OVER(ORDER BY order_date) AS previous_revenue, 
+LAG(order_date, 1) OVER(ORDER BY order_date) AS previous_month, 
+ from temp1 )
+
+select * from temp2 where 
+current_revenue - previous_revenue > 0 
+AND 
+previous_revenue IS NOT NULL
+AND 
+previous_month IS NOT NULL
+AND 
+datediff("month", previous_month, current_month) =1
+
+
+*/
