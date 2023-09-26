6430de8d92754167db0575d3eae4840e40d6bbff
aayushi-y <33841859+aayushi-y@users.noreply.github.com>
2023-09-26 12:48:02 +0530

    Update 9.sql

--- 9.sql
+++ 9.sql
@@ -1,3 +1,15 @@
+/*﻿
+Task:
+Calculate the running total of sales revenue, running_total, and the % of quota reached, percent_quota, for each sales employee on each date they make a sale. 
+Use the sales and employees table to pull in and create the following fields:
+salesemployeeid
+saledate
+saleamount
+quota
+running_total
+percent_quota
+Order the final output by sales employeeid and saledate. */
+
 WITH SALES_DATA AS (select salesemployeeid, saledate, saleamount, QUOTA, 
 SUM(SALEAMOUNT) OVER(PARTITION BY SALESEMPLOYEEID ORDER BY SALEDATE) as RUNNING_TOTAL
 from employees join sales 
