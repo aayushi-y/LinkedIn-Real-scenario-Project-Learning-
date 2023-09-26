c7bd59c376888ca676b1b879925c441fa27c7714
aayushi-y <33841859+aayushi-y@users.noreply.github.com>
2023-09-26 13:15:41 +0530

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,3 +1,19 @@
+/*
+Business Problem: Flagging upsell opportunities for the sales team
+The product team is launching a new product offering that can be added on top of a current subscription for an increase in the customer's annual fee. The sales team has decided
+that they first want to reach out to a select group of customers to offer the new product and get feedback before offering it to the entire customer base.
+﻿
+
+They've decided it would be best to reach out to customers who meet the following conditions:
+1. Have at least 5,000 registered users. Companies with large amounts of users are a significant upsell opportunity, because they can lead to more potential revenue. 
+(More users = More $)
+2. Only have one product subscription. Based on conversations the sales team has had with customers, the companies that already have subscriptions for two products are not 
+going to be willing to spend more and add onto their current subscriptions. You decide to help out the sales team by providing them a report with all current customers and 
+the number of product subscriptions and registered users they currently have. To make it extra easy for the sales team to identify customers that are upsell opportunity,
+you also include a binary column that flags the customers that meet these conditions.
+
+*/
+
 With temp1 As (select customerID, Sum(Numberofusers) AS total_users from subscriptions
 group by customerID),
 
