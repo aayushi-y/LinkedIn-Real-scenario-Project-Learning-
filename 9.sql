21bb98356c262acb3e9bf7c145a01a79e6394f37
aayushi-y <33841859+aayushi-y@users.noreply.github.com>
2023-09-26 12:49:32 +0530

    Update 9.sql

--- 9.sql
+++ 9.sql
@@ -1,4 +1,9 @@
 /*﻿
+Business problem: Tracking Sales Quota Progress over Time
+The sales team works diligently to sell the product, and they have quotas that they must reach in order to earn all of their commission. Because these goals are so intimately
+tied to revenue, the manager of the team wants to track each sales member's performance throughout the year. You suggest a % of quota reached metric that could be displayed 
+on a dashboard, but the sales manager expresses her concern that a single metric won't give her visibility into their progress throughout the year. You suggest providing a 
+running_total of sales revenue and a percent_quota metric that will be recalculated every time a sales member makes another sale. She agrees, and you get started!
 Task:
 Calculate the running total of sales revenue, running_total, and the % of quota reached, percent_quota, for each sales employee on each date they make a sale. 
 Use the sales and employees table to pull in and create the following fields:
